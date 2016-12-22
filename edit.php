4b76fc35cd6827b3816eb14339bbb9092ad76a5c
nuryanayu <ayunuryan@gmail.com>
2016-12-22 15:23:49 +0700

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,13 @@
+<?php 
+ $exif = exif_read_data('edit.jpg',null,true,true);
+ ?>
+ <pre>
+ 	<?php print_r($exif) ?>
+ </pre>
+
+ 
+<?php echo '<img src="data:'.$exif['COMPUTED']['Thumbnail.MimeType'].';base64,'.base64_encode($exif['THUMBNAIL']['THUMBNAIL']).'" alt="" />'; ?>
+
+
+ <!-- 
+ // http://fatfolderdesign.com/554/get-photo-exif-data-with-php -->
